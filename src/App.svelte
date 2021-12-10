7ef39aead444097da12986972eeccb3ead54038a
napthedev <phongna.dev@gmail.com>
2021-12-10 15:36:54 +0700

    fix underline animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
   import Intro from "./components/Intro.svelte";
   import Who from "./components/Who.svelte";
+  import Skills from "./components/Skills.svelte";
 </script>
 
 <main>
   <Intro />
   <Who />
+  <Skills />
 </main>
 
 <style>
---
+++ src/components/Skills.svelte
@@ -0,0 +1,8 @@
+<main />
+
+<style>
+  main {
+    height: 100vh;
+    background: #000;
+  }
+</style>
