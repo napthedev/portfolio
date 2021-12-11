e10490a6dc0940106d22749146d4c946d3a82252
napthedev <phongna.dev@gmail.com>
2021-12-11 20:58:24 +0700

    add see more button

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -26,6 +26,16 @@
   {#if extraTextVisible}
     <p in:slide>With a lot of knowledge collected</p>
   {/if}
+
+  {#if visible}
+    <div
+      on:click={() =>
+        window.scrollTo({ top: window.innerHeight, behavior: "smooth" })}
+      class="see-more"
+    >
+      <i in:fade={{ delay: 1600 }} class="bx bxs-chevrons-down" />
+    </div>
+  {/if}
 </div>
 
 <style>
@@ -44,5 +54,27 @@
     text-align: center;
     z-index: 10;
     margin: 0;
+  }
+
+  .see-more {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    cursor: pointer;
+    animation: up-down 1s infinite alternate;
+  }
+
+  i {
+    font-size: 50px;
+  }
+
+  @keyframes up-down {
+    from {
+      bottom: 35px;
+    }
+
+    to {
+      bottom: 60px;
+    }
   }
 </style>
